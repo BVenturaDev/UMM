a73339b47d0ba67289c96bb213a4431ff08275b2
Ben Ventura <oeslian@gmail.com>
2021-08-14 12:28:28 -0400

    Added camera controls

--- src/entities/control_camera.gd
+++ src/entities/control_camera.gd
@@ -1,7 +1,54 @@
 extends Camera
 
-const MAX_SPEED = 100.0
+const MAX_SPEED: float = 10.0
+const ZOOM_SPEED: float = 7.5
+const ROT_SPEED: float = 0.09
+const CAM_Y_MIN: float = 2.0
+const CAM_Y_MAX: float = 10.0
 
-func _process(var _delta: float) -> void:
-	#var in_dir: Vector2 =
-	pass
+var can_rot = false
+
+func _process(var delta: float) -> void:
+	# Get movement inputs
+	var in_dir: Vector2 = Vector2()
+	in_dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	in_dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	# Apply horizontal movement
+	var dir: Vector3 = Vector3()
+	dir += transform.basis.x * in_dir.x
+	dir += transform.basis.z * in_dir.y
+	dir = dir.normalized()
+	var h_vel: Vector3 = dir * MAX_SPEED * delta
+	transform.origin += h_vel
+	
+	# Apply zoom
+	if Input.is_action_just_released("ui_zoom_in") or Input.is_action_pressed("ui_zoom_in"):
+		transform.origin.y -= ZOOM_SPEED * delta
+	elif Input.is_action_just_released("ui_zoom_out")or Input.is_action_pressed("ui_zoom_out"):
+		transform.origin.y += ZOOM_SPEED * delta
+		
+	# Keep camera in range
+	if transform.origin.y < CAM_Y_MIN:
+		transform.origin.y = CAM_Y_MIN
+	elif transform.origin.y > CAM_Y_MAX:
+		transform.origin.y = CAM_Y_MAX
+		
+	# Camera Rotation
+	if Input.is_action_pressed("ui_right_click"):
+		# Capture the mouse for rotation
+		Globals.capture_mouse()
+		can_rot = true
+	else:
+		can_rot = false
+		Globals.free_mouse()
+		
+func _input(var event: InputEvent) -> void:
+	if event is InputEventMouseMotion and can_rot:
+		# Calculate camera rotation
+		rotate_y(deg2rad(-event.relative.x * ROT_SPEED))
+		rotate_x(deg2rad(-event.relative.y * ROT_SPEED))
+		# Keep camera from looking too far up or upside down
+		rotation.x = clamp(rotation.x, deg2rad(-90.0), deg2rad(45.0))
+		# Stop camera tilt
+		rotation.z = 0
--- src/singletons/globals.gd
+++ src/singletons/globals.gd
@@ -3,4 +3,12 @@ extends Node
 # Constants
 
 # Map size
-const MAP_SIZE: int = 100
+const MAP_SIZE: int = 50
+
+func capture_mouse() -> void:
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+func free_mouse() -> void:
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
