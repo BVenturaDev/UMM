88113675175d550b8c2fbd5bc8e31084de90f132
Jose Guevara <josexx1234@gmail.com>
2021-08-15 12:21:34 -0400

    Rename turn of state machine Add nature turn

---
+++ states/game/nature_turn.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var state_machine: StateMachine
+
+
+func enter():
+	assert(GameSignals.connect("next_turn", self, "_next_turn") == 0)
+	# Events on enter
+	GameSignals.emit_signal("enter_nature_turn")
+
+func exit():
+	GameSignals.disconnect("next_turn", self, "_next_turn")
+	# Events on exit
+	GameSignals.emit_signal("exit_nature_turn")
+
+#Can add the built functions if is defined in StateMachine class
+func _process(_delta: float) -> void:
+	pass
+
+func _next_turn() -> void:
+	state_machine.change_to("player_turn")
