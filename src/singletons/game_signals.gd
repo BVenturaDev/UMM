3485ba3f0b4982076fec5780c6e8d3572c4813be
Jose Guevara <josexx1234@gmail.com>
2021-08-15 10:04:07 -0400

    Add basic machinestate to gamestates

--- src/singletons/game_signals.gd
+++ src/singletons/game_signals.gd
@@ -1,4 +1,8 @@
-extends Object
+extends Node
 
 # warning-ignore:unused_signal
 signal next_turn
+# warning-ignore:unused_signal
+signal enter_player_turn
+# warning-ignore:unused_signal
+signal exit_player_turn
---
+++ states/game/turn_ai.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var state_machine: StateMachine
+
+
+func enter():
+	assert(GameSignals.connect("next_turn", self, "_next_turn") == 0)
+
+
+func exit():
+	GameSignals.disconnect("next_turn", self, "_next_turn")
+
+
+func _next_turn() -> void:
+	state_machine.change_to("turn_player")
