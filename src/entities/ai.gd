434abf225f0835159604f7a6a106b12b861484a2
BVenturaDev <oeslian@gmail.com>
2021-08-18 16:59:56 -0400

    AI gather shroom fix

--- src/entities/ai.gd
+++ src/entities/ai.gd
@@ -4,5 +4,5 @@ onready var fungus = $fungus
 
 func do_turn():
 	for tile in fungus.owned_tiles:
-		if tile.cur_resource and tile.tile_food.size() > 5 and not tile.turn_used:
+		if tile.cur_resource and tile.tile_food.size() > 5 and not tile.turn_used and not tile.cur_shroom:
 			tile.build_gather_shroom()
