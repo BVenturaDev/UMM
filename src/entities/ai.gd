b374b3251d65f8334be2248b476e38ca7eb5edf2
BVenturaDev <oeslian@gmail.com>
2021-08-19 15:27:44 -0400

    Fixed Shroom Cloning Bug

--- src/entities/ai.gd
+++ src/entities/ai.gd
@@ -4,5 +4,6 @@ onready var fungus = $fungus
 
 func do_turn():
 	for tile in fungus.owned_tiles:
-		if tile.cur_resource and tile.tile_food.size() > 5 and not tile.turn_used and not tile.cur_shroom:
-			tile.build_gather_shroom()
+		if tile.cur_resource and not tile.turn_used and not tile.cur_shroom:
+			if tile.tile_food.size() > 5:
+				tile.build_gather_shroom()
