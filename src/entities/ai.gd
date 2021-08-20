2876b4720043ed0af81039d04096233dd29256c0
BVenturaDev <oeslian@gmail.com>
2021-08-20 11:24:43 -0400

    Enemy AI now makes scout shrooms

--- src/entities/ai.gd
+++ src/entities/ai.gd
@@ -32,6 +32,36 @@ func do_turn():
 						if _try_gather_food(tile):
 							tile.build_poison_shroom()
 				# Check if we need a scout shroom
+				var neighbors: Array = Globals.grid.find_neighbors(tile.x, tile.y)
+				var has_scout: bool = false
+				var need_scout: bool = false
+				for neighbor in neighbors:
+					# Check if owned and has shroom
+					if neighbor.owner_fungus:
+						if neighbor.owner_fungus == self:
+							if neighbor.cur_shroom:
+								if neighbor.cur_shroom.is_in_group("scout"):
+									has_scout = true
+						else:
+							need_scout = true
+				if not has_scout and need_scout:
+					if tile.tile_food.size() > 5:
+						tile.build_scout_shroom()
+					else:
+						if _try_gather_food(tile):
+							tile.build_scout_shroom()
+		if tile.cur_shroom:
+			if tile.cur_shroom.is_in_group("scout"):
+				var neighbors: Array = Globals.grid.find_neighbors(tile.x, tile.y)
+				var enemy_neighbors: bool = false
+				for neighbor in neighbors:
+					if neighbor.owner_fungus:
+						if not neighbor.owner_fungus == self:
+							# Enemy revealed (maybe attack here)
+							neighbor.revealed = true
+							enemy_neighbors = true
+				if not enemy_neighbors:
+					tile.cur_shroom.kill()	
 
 func _try_gather_food(var tile: Object) -> bool:
 	var region: Array = Globals.grid.find_region(tile.x, tile.y)
