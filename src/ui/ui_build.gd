9d8af3ea4f43d387f786b94a24ef65bbd169d42b
Jose Guevara <josexx1234@gmail.com>
2021-08-20 16:38:45 -0400

    Automatic move food for the tile needed

--- src/ui/ui_build.gd
+++ src/ui/ui_build.gd
@@ -25,7 +25,7 @@ var selected_tile: Object = null
 func _ready() -> void:
 	Globals.build_ui = self
 
-func make_build_menu(var cur_food: int, var tile: Object) -> void:
+func make_build_menu(var cur_food: int, var tile: Tile) -> void:
 	if Globals.DEBUG and tile.cur_shroom:
 		kill_shroom.visible = true
 	else:
@@ -46,7 +46,7 @@ func make_build_menu(var cur_food: int, var tile: Object) -> void:
 		poison_shroom.visible = false
 		scout_shroom.visible = false
 		
-	if cur_food > 2:
+	if tile.food_amount > 2:
 		slider_cont.visible = true
 		move_food_butt.visible = true
 		if cur_food > 5 and tile.enemies.size() > 0:
@@ -62,12 +62,12 @@ func make_build_menu(var cur_food: int, var tile: Object) -> void:
 		attack_butt.visible = false
 		
 	tile_food = cur_food
-	cur_food_text.text = str(cur_food)
-	if cur_food <= MAX_FOOD_MOVE:
-		food_slider.max_value = cur_food - 1
-		food_slider.value = cur_food - 1
-		attack_slider.max_value = cur_food - 1
-		attack_slider.value = cur_food - 1
+	cur_food_text.text = str(tile.food_amount)
+	if tile.food_amount <= MAX_FOOD_MOVE:
+		food_slider.max_value = tile.food_amount - 1
+		food_slider.value = tile.food_amount - 1
+		attack_slider.max_value = tile.food_amount - 1
+		attack_slider.value = tile.food_amount - 1
 	else:
 		food_slider.max_value = MAX_FOOD_MOVE
 		food_slider.value = MAX_FOOD_MOVE
