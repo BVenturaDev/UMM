1688a27a201e338e2a0bf312f2283c8ec1aab288
BVenturaDev <oeslian@gmail.com>
2021-08-21 19:22:25 -0400

    Added menu navigation

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -1,6 +1,9 @@
 extends Spatial
 
 onready var light = $WorldEnvironment/DirectionalLight
+onready var options = $CanvasLayer/options_menu
+
+var main_level = preload("res://scenes/levels/main_level.tscn")
 
 func _ready():
 	if Globals.BUILD_MOBILE:
@@ -10,3 +13,14 @@ func _ready():
 
 func _on_Timer_timeout():
 	GameSignals.emit_signal("next_turn")
+
+
+func _on_options_button_pressed():
+	options.visible = true
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_play_button_pressed():
+	var _a = get_tree().change_scene_to(main_level)
